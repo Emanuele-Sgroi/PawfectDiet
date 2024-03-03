60cb8b029c93ca5608852255903c418bb92dd51a
Emanuele Sgroi <nunzioemanuelesgroi@gmail.com>
2024-03-03 11:01:06 +0000

    Added large button component

--- src/components/buttons/ButtonLarge.js
+++ src/components/buttons/ButtonLarge.js
@@ -1,56 +1,52 @@
-import React, { useState } from "react";
+/**
+ * ButtonLarge
+ * -----------
+ * Branded filled button. Shows an optional right arrow icon.
+ */
+import React from "react";
 import { View, StyleSheet, Text, TouchableHighlight } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome6";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
 
-const ButtonLarge = ({ buttonName, isThereArrow, onPress }) => {
-  return (
-    <TouchableHighlight
-      underlayColor="#1d2245"
-      onPress={onPress}
-      style={styles.buttonContainer}
-    >
-      <View style={styles.buttonSubContainer}>
-        <Text style={styles.textButtonStyle}>{buttonName}</Text>
-        {isThereArrow && (
-          <Icon
-            name="arrow-right-long"
-            size={30}
-            color="#FFFFFF"
-            style={styles.iconStyle}
-          />
-        )}
-      </View>
-    </TouchableHighlight>
-  );
-};
+const ButtonLarge = ({ buttonName, isThereArrow = false, onPress }) => (
+  <TouchableHighlight
+    underlayColor="#1d2245"
+    onPress={onPress}
+    style={styles.container}
+  >
+    <View style={styles.inner}>
+      <Text style={styles.label}>{buttonName}</Text>
+      {isThereArrow && (
+        <FontAwesome6
+          name="arrow-right-long"
+          size={30}
+          color="#FFFFFF"
+          style={styles.icon}
+        />
+      )}
+    </View>
+  </TouchableHighlight>
+);
 
 const styles = StyleSheet.create({
-  buttonContainer: {
+  container: {
     backgroundColor: "#273176",
     width: "100%",
     height: 39,
     borderRadius: 20,
-    marginVertical: 10,
-    marginTop: 20,
+    marginVertical: 20,
   },
-  buttonSubContainer: {
+  inner: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    width: "100%",
     height: "100%",
   },
-  textButtonStyle: {
+  label: {
     fontSize: 20,
     fontFamily: "MerriweatherSans-Bold",
-    padding: 0,
-    textAlignVertical: "center",
     color: "#FFFFFF",
-    includeFontPadding: false,
   },
-  iconStyle: {
-    marginLeft: 10,
-  },
+  icon: { marginLeft: 10 },
 });
 
 export default ButtonLarge;
